5956fa84d495d5c9532401812664844ee466b2aa
satooru65536 <ryuto4619@gmail.com>
2024-04-25 11:56:26 +0900

    chore: storybook のセットアップ

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.stories.@(ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+  webpackFinal(config) {
+    config.resolve!.plugins = [...(config.resolve!.plugins || []), new TsconfigPathsPlugin()];
+    return config;
+  },
+};
+export default config;
