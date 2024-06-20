c5e8f0f301f1e4f2dcf68cc3170e4e072198b3b4
satooru65536 <ryuto4619@gmail.com>
2024-06-20 09:59:28 +0900

    feat: ImageButtonエレメント

---
+++ src/elements/ImageButton/index.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import logo from 'public/logo/logo.png';
+import ImageButton from '.';
+
+const meta: Meta<typeof ImageButton> = {
+  component: ImageButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof ImageButton>;
+
+export const Circle: Story = {
+  args: {
+    radius: 'circle',
+    src: logo.src,
+    width: 100,
+    height: 100,
+    style: { border: 'solid' },
+  },
+};
+
+export const Rounded: Story = {
+  args: {
+    radius: 'md',
+    src: logo.src,
+    width: 100,
+    height: 100,
+    style: { border: 'solid' },
+  },
+};
+
+export const RoundedSmall: Story = {
+  args: {
+    radius: 'sm',
+    src: logo.src,
+    width: 100,
+    height: 100,
+    style: { border: 'solid' },
+  },
+};
