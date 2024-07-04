a833a9831ff96527028e5a39bc3c94c275ac1802
karasuneo <yusei4520030401@gmail.com>
2024-07-04 10:27:35 +0900

    add : フラグメント検索ページの作成

---
+++ src/components/FragmentSearch/index.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import FragmentSearch from '.';
+
+const meta: Meta<typeof FragmentSearch> = {
+  component: FragmentSearch,
+};
+
+export default meta;
+type Story = StoryObj<typeof FragmentSearch>;
+
+export const Default: Story = {};
